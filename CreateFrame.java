2a9a5ccc9582e63e800b9389d4259fe579fd362c
RJ Hollinger <33728865+roho2@users.noreply.github.com>
2021-03-26 13:20:08 -0400

    Refactored the ButtonHandler class

--- CreateFrame.java
+++ CreateFrame.java
@@ -53,13 +53,11 @@ public class CreateFrame extends JFrame {
         setUpPanels();
     }
 
-
     private void setUpPanels(){
         dbPanel = new DBPanel();
         comPanel = new CommandPanel();
         resultPanel = new ResultPanel();
     }
-
 
     private void setConstraints(){
         Constraints constraint = new Constraints();
@@ -71,7 +69,6 @@ public class CreateFrame extends JFrame {
         constraint.setConstraintsForResultPanel();
         this.add(resultPanel, constraint.getConstraint());
     }
-
 
     private void createButtonActionListeners() {
         resultPanel.buttons.clearCommand.addActionListener(bh);
@@ -96,52 +93,57 @@ public class CreateFrame extends JFrame {
         }
 
         public void actionPerformed(ActionEvent e) {
-
-            //Connect to database
             switch (e.getActionCommand()) {
-                case "connect":
-                    try {
-                        Class.forName("com.mysql.cj.jdbc.Driver");
-                        myConnection = DriverManager.getConnection(mainFrame.dbPanel.getURL(), mainFrame.dbPanel.getUsername(), mainFrame.dbPanel.getPassword());
-                        mainFrame.resultPanel.buttons.status.setText("Connected to jdbc:mysql:127.0.0.1:3306");
-                    } catch (SQLException | ClassNotFoundException e1) {
-                        JOptionPane.showMessageDialog(null, "Check login information and try again", "Connection Error", JOptionPane.WARNING_MESSAGE);
-                    }
-                    break;
-                //Clear command window in top right
-                case "clearCommand":
-                    mainFrame.comPanel.clearCommandText();
-                    break;
-                //Clear result window at bottom
-                case "clearWindow":
-                    mainFrame.resultPanel.clearResultText();
-                    break;
-                //Execute command that is in command text box
-                case "execute":
-
-                    try {
-                        if (myConnection == null) {
-                            JOptionPane.showMessageDialog(null, "Must connect to database before executing commands", "Connection Error", JOptionPane.WARNING_MESSAGE);
-                            return;
-                        }
-                        Statement sql = myConnection.createStatement();
-                        boolean resultSetBool = sql.execute(mainFrame.comPanel.getCommandText());
-
-                        //If true, statement is a query, else it is a data manipulation command
-                        if (resultSetBool) {
-                            //Create result set, populate a JTable with it and send to printing method
-                            ResultSet rs = sql.executeQuery(mainFrame.comPanel.getCommandText());
-                            JTable table = new JTable(BuildTableModel.buildTableModel(rs));
-                            mainFrame.resultPanel.setOutputText(table);
-                        } else {
-                            JOptionPane.showMessageDialog(null, "Successfully updated table", "Success", JOptionPane.WARNING_MESSAGE);
-                        }
-
-                    } catch (SQLException e1) {
-                        JOptionPane.showMessageDialog(null, e1.getMessage(), "Database Error", JOptionPane.WARNING_MESSAGE);
-                    }
-                    break;
+                case "connect" -> tryConnection();
+                case "clearCommand" -> mainFrame.comPanel.clearCommandText();
+                case "clearWindow" -> mainFrame.resultPanel.clearResultText();
+                case "execute" -> executeCommand();
             }
-        }//End of method
-    }//End of listener class
-}//End of CreateFrame class
+        }
+
+        private void tryConnection(){
+            try {
+                createConnection();
+            } catch (SQLException | ClassNotFoundException e1) {
+                JOptionPane.showMessageDialog(null, "Check login information and try again", "Connection Error", JOptionPane.WARNING_MESSAGE);
+            }
+        }
+
+        private void createConnection() throws ClassNotFoundException, SQLException{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            myConnection = DriverManager.getConnection(mainFrame.dbPanel.getURL(), mainFrame.dbPanel.getUsername(), mainFrame.dbPanel.getPassword());
+            mainFrame.resultPanel.buttons.status.setText("Connected to jdbc:mysql:127.0.0.1:3306");
+        }
+
+        private void executeCommand(){
+            try {
+                if (myConnection == null) {
+                    JOptionPane.showMessageDialog(null, "Must connect to database before executing commands", "Connection Error", JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
+                runCommand();
+            } catch (SQLException e1) {
+                JOptionPane.showMessageDialog(null, e1.getMessage(), "Database Error", JOptionPane.WARNING_MESSAGE);
+            }
+        }
+
+        private void runCommand() throws SQLException{
+            Statement sql = myConnection.createStatement();
+            boolean doesCommandReturnResultSet = sql.execute(mainFrame.comPanel.getCommandText());
+
+            if (doesCommandReturnResultSet) {
+                executeQuery(sql);
+            } else {
+                JOptionPane.showMessageDialog(null, "Successfully updated table", "Success", JOptionPane.WARNING_MESSAGE);
+            }
+        }
+
+        private void executeQuery(Statement sql) throws SQLException{
+            ResultSet rs = sql.executeQuery(mainFrame.comPanel.getCommandText());
+            JTable table = new JTable(BuildTableModel.buildTableModel(rs));
+            mainFrame.resultPanel.setOutputText(table);
+        }
+
+    }
+
+}
